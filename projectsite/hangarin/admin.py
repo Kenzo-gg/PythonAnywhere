6d591d72416eb55547c423f2db360da6f58a6a08
Kenzo-gg <k3nzkie122@gmail.com>
2026-08-17 10:48:48 +0800

    added initial data

--- projectsite/hangarin/admin.py
+++ projectsite/hangarin/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, Priority, Category, Note, SubTask
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('task_title', 'status', 'task_deadline', 'task_priority', 'task_category', )
+    search_fields = ('task_title', 'task_description', )
+    list_filter = ('status', 'task_priority', 'task_category', )
+
+@admin.register(Priority)
+class PriorityAdmin(admin.ModelAdmin):
+    list_display = ('priority_name',)
+    search_fields = ('priority_name',)
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name',)
+    search_fields = ('category_name',)
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):  
+    list_display = ('parent_task_note', 'note_content', 'created_at',)
+    search_fields = ('note_content',)
+    list_filter = ('created_at',)
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('subtask_title', 'subtask_status', 'parent_task_subtask',)
+    search_fields = ('subtask_title',)
--- projectsite/hangarin/models.py
+++ projectsite/hangarin/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Priority(BaseModel):
+    priority_id = models.IntegerField(primary_key=True)
+    priority_name = models.CharField(max_length=150)
+
+    def __str__(self): return self.priority_name
+
+class Category(BaseModel):
+    category_id = models.IntegerField(primary_key=True)
+    category_name = models.CharField(max_length=150)
+
+    def __str__(self): return self.category_name
+
+class Task(BaseModel):
+    task_id = models.IntegerField(primary_key=True)
+    task_title = models.CharField(max_length=150)
+    task_description = models.TextField(max_length=500)
+    task_deadline = models.DateTimeField()
+    status = models.CharField(max_length=50,choices=[("Pending", "Pending"),("In Progress ", "In Progress"),("Completed", "Completed"),],default="pending")
+    task_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='category')
+    task_priority = models.ForeignKey(Priority, on_delete=models.CASCADE, null=True, related_name='priority')
+
+    def __str__(self): return self.task_title
+
+class SubTask(BaseModel):
+    subtask_id = models.IntegerField(primary_key=True)
+    parent_task_subtask = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='subtasks')
+    subtask_title = models.CharField(max_length=255)
+    subtask_status = models.CharField(max_length=50,choices=[("Pending", "Pending"),("In Progress ", "In Progress"),("Completed", "Completed"),],default="pending")
+
+    def __str__(self): return self.subtask_title
+
+class Note(BaseModel):
+    note_id = models.IntegerField(primary_key=True)
+    parent_task_note = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='notes')
+    note_content = models.TextField(max_length=500)
+
+    def __str__(self): return self.note_content
+
+
+
